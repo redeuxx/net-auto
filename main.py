abb797075f25f483ccb319e54a7a36eab166d732
Vernon Wenberg <vwenberg@gmail.com>
2021-05-02 12:59:13 -0500

    test commit again

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import settings
 
 
 def main():
-    connection = Netmiko(settings.host, settings.port, settings.username, settings.password, settings.device_type)
+    connection = Netmiko(settings.host, settings.port, settings.username, settings.password,
+                         device_type=settings.device)
     output = connection.send_command("show run")
     print(output)
     connection.disconnect()
